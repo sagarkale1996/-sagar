68436d62cf5e375feb826f674a65f3285fe812b7
sagarkale1996 <71489239+sagarkale1996@users.noreply.github.com>
2021-02-13 21:41:14 +0530

    Intial commit

--- blog/middlewares.py
+++ blog/middlewares.py
@@ -9,6 +9,10 @@
     return my_function'''
 
 class MyMiddlewares:
+
+    def Komal(self):
+        pass
+
     def __init__(self,get_response):
         self.get_response=get_response
         print("One time Intialization")
