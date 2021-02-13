12738b5a833fa0fa746169bb34efdfce4bc2cda7
sagarkale1996 <71489239+sagarkale1996@users.noreply.github.com>
2021-02-13 20:09:26 +0530

    all task is completewd initail commit

---
+++ blog/middlewares.py
@@ -0,0 +1,21 @@
+'''def my_middleware(get_response):
+    print("one time execution")
+
+    def my_function(request):
+        print("this is before view")
+        response=get_response(request)
+        print("this is after view")
+        return response
+    return my_function'''
+
+class MyMiddlewares:
+    def __init__(self,get_response):
+        self.get_response=get_response
+        print("One time Intialization")
+
+    def __call__(self,request):
+        print("This is before view")
+        response= self.get_response(request)
+        print("This is after view")
+        return response
+
